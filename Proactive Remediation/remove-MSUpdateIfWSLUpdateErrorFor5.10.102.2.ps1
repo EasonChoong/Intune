446842d0b3723389ed9fe6f4b4c1e2647baad7f2
Eason Choong Yee Siang <easonchoong@zfx.com>
2025-10-06 12:06:08 +0800

    Update

---
+++ Proactive Remediation/remove-MSUpdateIfWSLUpdateErrorFor5.10.102.2.ps1
@@ -0,0 +1,10 @@
+﻿$searchTitle = "Windows Subsystem for Linux Update - 5.10.102.2"
+$subsystem = dism /online /get-features /format:table | findstr /i "Subsystem"
+
+$target = Get-WindowsUpdate -MicrosoftUpdate -IgnoreUserInput | Where-Object { $_.Title -eq $searchTitle }
+
+if(!$subsystem -and $target){
+
+}else{
+    #Do nothing
+}
